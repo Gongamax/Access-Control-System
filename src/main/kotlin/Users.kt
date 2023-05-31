7f93340bccfd5434ec14cebb47d09f0b4cd2c30e
Francisco Saraiva <a49462@alunos.isel.pt>
2023-05-31 14:53:34 +0100

    Some corrections in MSG

--- src/main/kotlin/Users.kt
+++ src/main/kotlin/Users.kt
@@ -1,13 +1,16 @@
 import java.security.MessageDigest
 
-data class User(val uin: Int, val name: String, var pin: String, var message : String = "")
+data class User(val uin: Int, val name: String, var pin: String, var message: String = "")
 
-const val MAX_USERS = 1000
+const val MAX_USERS = 100
 
 class Users(private val maxSize: Int = MAX_USERS) {
 
     private val users: HashMap<Int, User> = HashMap(maxSize)
 
+    /**
+     * Função que adiciona um utilizador
+     */
     fun addUser(name: String, pin: String) {
         if (users.size >= MAX_USERS) {
             error("Maximum number of users reached")
@@ -15,23 +18,29 @@ class Users(private val maxSize: Int = MAX_USERS) {
         val encryptedPIN = encryptPIN(pin)
         val uin = generateUIN()
         val newUser = User(uin, name, encryptedPIN)
-        println(newUser)
         users.put(uin, newUser)
-        println("Utilizador adicionado. UIN: $uin")
+        println("Adding user $uin:$name")
     }
 
+    /**
+     * Função que remove um utilizador
+     */
     fun removeUser(uin: Int) {
-        if (users.containsKey(uin)) {
+        val name = getAllUsers()[uin].name
+        if (users.containsKey(uin)){
             users.remove(uin)
-            println("Utilizador removido. UIN: $uin")
-        } else {
-            println("Utilizador não encontrado. UIN: $uin")
+            println("User $uin:$name removed")
         }
+
     }
 
+
+    /**
+     * Função que altera o PIN
+     */
     fun changePin(uin: Int, newPin: String) {
         val users = getAllUsers()
-        if (users.any { it.uin==uin }) {
+        if (users.any { it.uin == uin }) {
             val encryptedPIN = encryptPIN(newPin)
             users[uin].pin = encryptedPIN
             saveUsersToFile()
@@ -41,11 +50,13 @@ class Users(private val maxSize: Int = MAX_USERS) {
         }
     }
 
+    /**
+     * Função que trata da mensagem
+     */
     fun changeMessage(uin: Int, message: String) {
         val users = getAllUsers()
         if (users.any { it.uin == uin }) {
             users[uin].message = message
-            saveUsersToFile()
             if (message != "") {
                 println("The message \"$message\" has been associated to $uin:${users[uin].name}")
             }
@@ -54,15 +65,25 @@ class Users(private val maxSize: Int = MAX_USERS) {
         }
     }
 
+
+    /**
+     * Função que gera um UIN
+     */
+
     private fun generateUIN(): Int {
         val users = getAllUsers()
-        val lastUin = if (users.isNotEmpty()) users.maxBy { it.uin }.uin else -1
-        if (lastUin < MAX_USERS)
-            return lastUin + 1 else error("No more users avaiable")
+        val assignedUin = HashSet(users.map { it.uin })
+        val availableUin = (0..MAX_USERS).filterNot { assignedUin.contains(it) }
+        if (availableUin.isNotEmpty())
+            return availableUin.first()
+        else error("No more users avaiable")
     }
 
-    // Obter todos os utilizadores
-   fun getAllUsers(): List<User> {
+
+    /**
+     * Função que obtem todos os utilizadores
+     */
+    fun getAllUsers(): List<User> {
         FileAccess.readTextFile("MY_USERS.txt").forEach {
             if (it.isEmpty()) return@forEach
             val (uin, pin, name, message) = it.split(";")
@@ -72,19 +93,30 @@ class Users(private val maxSize: Int = MAX_USERS) {
         return users.values.toList()
     }
 
+    /**
+     * Função que autentica o utilizador
+     */
     fun authenticateUser(uin: String, pin: String): User? {
         val list = Users().getAllUsers()
         return list
             .firstOrNull { user -> user.uin == uin.toInt() && user.pin == Users().encryptPIN(pin) }
     }
 
-    //Esta função vai ser chamada no App.kt
+    /**
+     * Função que obtem o utilizador
+     *
+     * Esta função vai ser chamada no App.kt
+     */
+
     fun saveUsersToFile() {
         val content = users.values.toList()
             .joinToString("\n") { "${it.uin};${it.pin};${it.name};${it.message};" }
         FileAccess.writeFileFromZero("MY_USERS.txt", content)
     }
 
+    /**
+     * Função que encripta o PIN
+     */
     fun encryptPIN(pin: String): String {
         val md = MessageDigest.getInstance("SHA-256")
         val hashedBytes = md.digest(pin.toByteArray())
@@ -99,7 +131,7 @@ fun main() {
     users.saveUsersToFile()
     //val usersList = users.getAllUsers().toString()
     //println(usersList)
-   // val user = users.addUser("Manuel", "1111")
+    // val user = users.addUser("Manuel", "1111")
     //users.saveUsersToFile()
     val usersList = users.getAllUsers().toString()
     val a = users.getAllUsers()
